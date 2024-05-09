86aa9b2b12c2c965790a9fb32bb8d83370a02d96
João Pedro Tomé <joaopedrot6@gmail.com>
2024-05-09 20:29:52 -0300

    Criado sistema de rotas Criado uma pagina Home.js Criado uma pagina ProdutoCreate.js As duas somente com o nome do arquivo.
    Sistema de rotas utilizando react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ProdutoCreate from './pages/Produto/produtoCreate'; 
+import Home from './Home'
+
+//Aqui é a confiuração de rotas da pagina.
+//para cada objeto na lista, é um caminho
+//Path: Caminho no URL da pagina para acessar
+//element: O elemento/Pagina para carregar ao acessar a URL
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home/>
+    },
+    {
+      path: "/Produto",
+      element: <ProdutoCreate/>
+    }
+  ]
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/pages/Produto/produtoCreate.js
@@ -0,0 +1,15 @@
+// import './App.css';
+
+function ProdutoCreate() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Criar Produto
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default ProdutoCreate;
