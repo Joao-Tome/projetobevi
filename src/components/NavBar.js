34b60273d6e87dfcc6204e69f66442357edc44f7
João Pedro Tomé <joaopedrot6@gmail.com>
2024-05-11 15:06:57 -0300

    Criado o NavBar da pagina

---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import { Navbar, Nav, Container } from "react-bootstrap"
+import "../stylesheet/NavBar.css"
+
+function NavBar(){
+    return (
+    <>
+        <Navbar expand="sm" className="navbar-color">
+            <Container>
+            <Navbar.Brand href="#home">Projeto Teste Bevi</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto" navbarScroll>
+                        <Nav.Link>
+                            Home
+                        </Nav.Link>
+                        <Nav.Link>
+                            Produto
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </>
+    )
+}
+
+export default NavBar
