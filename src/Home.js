efe3e50ea8cbe7f194eb9dfb15b30177db416a9d
João Pedro Tomé <joaopedrot6@gmail.com>
2024-05-11 11:50:36 -0300

    Teste da Api Feito! Adicionado Sweet Alerts 2 Login e Teste com o metodo Me Feito!

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {instanceAxios, AddApiKey} from './services/axios.js'
+import Swal from 'sweetalert2';
+
+async function TestarAPI(){
+  instanceAxios.post('/auth/me')
+  .then((resp) => {
+    Swal.fire("Deu certo")
+  })
+  .catch((error) => {
+    Swal.fire("Deu errado")
+  })
+
+}
+
+async function LogarAPI(){
+  instanceAxios({
+    method: 'post',
+    url: "/auth/login",
+    data: {
+      "email": process.env.REACT_APP_EMAIL_API,
+      "password": process.env.REACT_APP_SENHA_API
+    }
+  })
+  .then((resp) => {
+    console.log(resp)
+    if (resp.data.access_token != null){
+      AddApiKey(resp.data.access_token)
+    }
+  })
+  .catch((error) => {
+    console.log(error)
+    Swal.fire("Deu errado")
+  })
+}
 
 function Home() {
   return (
@@ -10,6 +44,14 @@ function Home() {
           Home
         </p>
       </header>
+      <body>
+        <button type='button' onClick={() => LogarAPI()}>
+          Logar
+        </button>
+        <button type='button' onClick={() => TestarAPI()}>
+          Testar API com o metodo ME
+        </button>
+      </body>
     </div>
   );
 }
--- src/services/axios.js
+++ src/services/axios.js
@@ -4,6 +4,12 @@ const configAxios = {
     baseURL: process.env.REACT_APP_BASE_URL
 }
 
-const instanceAxios = axios.create(configAxios);
+export const instanceAxios = axios.create(configAxios);
+
+export function AddApiKey(ApiKey){
+    if (ApiKey != null){
+        instanceAxios.defaults.headers.common['Authorization'] = 'Bearer ' + ApiKey
+    }
+}
 
 export default instanceAxios;
